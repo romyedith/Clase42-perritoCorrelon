14228c7749fc521b45b6c0b3fdda9c106340cf05
romyedith <romyedith@gmail.com>
2022-11-11 19:21:48 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# C41-Plantilla-Código
+Plantilla del código para mover al perro asincrónicamente.
---
+++ sketch.js
@@ -0,0 +1,62 @@
+
+var dog;
+var dogpos;
+var database;
+var position;
+
+function preload(){
+    backdrop=loadImage("background.jpg");
+    dogimg=loadAnimation("dog1.png","dog2.png","dog3.png");
+    foodimg=loadImage("hotdog.png");
+}
+function setup(){    
+    createCanvas(500,500);
+    database = firebase.database();
+    dog=createSprite(200,200,100,100);
+    dog.addAnimation("running",dogimg);
+    dog.scale=0.5;
+    food=createSprite(450,450);
+    food.addImage(foodimg);
+    food.scale=0.5;
+
+    //.ref() y .on()
+    dogpos=database.ref("dog/position");
+    dogpos.on("value", readposition);
+}
+
+function draw(){
+    background(backdrop);
+    
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+       
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+       database.ref("dog/position").update({
+        x: position.x + x,
+        y: position.y + y
+       })
+
+
+}
+
+
+//Función para leer la posición.
+function readposition(data){
+    position = data.val();
+    console.log(position);
+    dog.x = position.x;
+    dog.y = position.y;
+}
